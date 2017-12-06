0775bc5eeedf81bbc09538ca52af05125a5ef415
Raja Jaganathan <rajajflex@gmail.com>
2017-12-06 12:34:08 +0530

    prop name changed

--- render-if.js
+++ render-if.js
@@ -1,12 +1,12 @@
 import { PropTypes } from 'react';
 
 functon RenderIf(props) => {
-  return props.test ? props.children : null;
+  return props.if ? props.children : null;
 };
 
 RenderIf.propTypes = {
   children: PropTypes.element.isRequired,
-  test: PropTypes.bool.isRequired
+  if: PropTypes.bool.isRequired
 };
 
 export default RenderIf;
